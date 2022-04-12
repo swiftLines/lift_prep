d9d2ab7ba82d949d144b744964fe5503ab97cba0
Jeremy Underwood <underwood.j248@gmail.com>
2022-04-11 21:45:22 -0400

    add view to update workout

--- myapp/lift_posts/views.py
+++ myapp/lift_posts/views.py
@@ -23,3 +23,26 @@ def create_post():
 def lift_post(lift_post_id):
     lift_post = Workout.query.get_or_404(lift_post_id) 
     return render_template('lift_post.html', title=lift_post.title, date=lift_post.date, post=lift_post)
+
+@lift_posts.route('/<int:lift_post_id>/update',methods=['GET','POST'])
+@login_required
+def update(lift_post_id):
+    lift_post = Workout.query.get_or_404(lift_post_id)
+
+    if lift_post.author != current_user:
+        abort(403)
+
+    form = LiftPostForm()
+
+    if form.validate_on_submit():
+        lift_post.title = form.title.data
+        lift_post.text = form.text.data
+        db.session.commit()
+        flash('Lift Post Updated')
+        return redirect(url_for('lift_posts.lift_post',lift_post_id=lift_post.id))
+
+    elif request.method == 'GET':
+        form.title.data = lift_post.title
+        form.text.data = lift_post.text
+
+    return render_template('create_post.html',title='Updating',form=form)
