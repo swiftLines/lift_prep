b2df701a798964fbcf117b0877745fb8b4cd6b92
Jeremy Underwood <underwood.j248@gmail.com>
2022-04-11 20:04:50 -0400

    add SQLAlchemy associatios

--- myapp/models.py
+++ myapp/models.py
@@ -18,6 +18,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Workout', backref='author', lazy=True)
 
     def __init__(self, email, username, password):
         self.email = email
@@ -31,14 +32,15 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"Username {self.username}"
 
-class LiftPost(db.Model):
-    __tablename__ = 'lift_posts'
+class Workout(db.Model):
+    __tablename__ = 'workout'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, nullable=False)
     title = db.Column(db.String(32), nullable=False)
     lift = db.Column(db.Text, nullable=False)
     sets = db.Column(db.Integer)
     reps = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     def __init__(self, date, title, lift, sets, reps, user_id):
         self.date = date
